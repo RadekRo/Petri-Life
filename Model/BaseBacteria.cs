740d817f8682444a3011a8f2c1fe71e93e75efe4
Radek Rocławski <r.roclawski@ankara.pl>
2023-09-27 18:21:20 +0200

    Copy

--- Model/BaseBacteria.cs
+++ Model/BaseBacteria.cs
@@ -1,26 +1,30 @@
 ﻿using Petri_Life.Enum;
 using System;
+using System.Runtime;
 
 namespace Petri_Life.Model
 {
     public class BaseBacteria
     {
         public int LifeSpan { get; set; }
+        public int InitialLifeSpan { get; set; }
         public int Nearby { get; set; }
         public int PositionX { get; set; }
         public int PositionY { get; set; }
-        public BacteriaTypes BacteriaType {  get; set; }
+        public BacteriaTypes BacteriaType { get; set; }
         public BaseBacteria(int positionX, int positionY, BacteriaTypes bacteriaType)
         {
             PositionX = positionX;
             PositionY = positionY;
             BacteriaType = bacteriaType;
         }
-        public BaseBacteria ShallowCopy()
+
+        public BaseBacteria DeepCopy()
         {
-            return (BaseBacteria)this.MemberwiseClone();
+            BaseBacteria other = (BaseBacteria)this.MemberwiseClone();
+            other.LifeSpan = this.InitialLifeSpan;
+            return other;
         }
     }
-
 }
 
